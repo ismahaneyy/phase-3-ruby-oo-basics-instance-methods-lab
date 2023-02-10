ebd41e7bb95b94da7f7d481f6f601b33ac1f217b
ismahaneyy <liciousmayrah@gmail.com>
2023-02-10 21:50:24 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+
+snoopy = Dog.new
+snoopy.bark
+
+fido = Dog.new
+fido.sit 
